a6b5608f6864ce061b8b75a8fde3b146a2717231
Ryan Alameddine <rhalameddine@gmail.com>
2018-06-10 12:31:42 -0700

    Added start of a Maze random generation and traversal program

---
+++ MazeTraversal/MazeTraversal/Board.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeTraversal
+{
+    public class Board
+    {
+        public byte[,] spots;
+        public List<Vector2> overLaySpots = new List<Vector2>();
+        public int width;
+        public int height;
+        public int size;
+
+        public Board(int width, int height, int size)
+        {
+            this.width = width;
+            this.height = height;
+            this.size = size;
+            spots = new byte[width, height];
+            for(int i = 0; i < width; i++)
+            {
+                for(int j = 0; j < height; j++)
+                {
+                    spots[i, j] = 0;
+                }
+            }
+        }
+
+        public Vector2 findStart()
+        {
+            for(int x = 0; x < width; x++)
+            {
+                for(int y = 0; y < height; y++)
+                {
+                    if(spots[x, y] == 2)
+                    {
+                        return new Vector2(x, y);
+                    } 
+                }
+            }
+            return new Vector2(-1, -1);
+        }
+
+        public Vector2 findEnd()
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (spots[x, y] == 3)
+                    {
+                        return new Vector2(x, y);
+                    }
+                }
+            }
+            return new Vector2(-1, -1);
+        }
+
+        List<(Vector2, float, Vector2)> getConnections(Vector2 point)
+        {
+            List<(Vector2, float, Vector2)> availableConnections = new List<(Vector2, float, Vector2)>();
+
+            for(int x = (int)point.X - 1; x <= point.X + 1; x++)
+            {
+                if (x < 0 || x >= width) { continue; }
+                for (int y = (int)point.Y - 1; y <= point.Y + 1; y++)
+                {
+                    if (y < 0 || y >= height) { continue; }
+                    if (x == point.X && y == point.Y) { continue; }
+
+                    availableConnections.Add((point, 0, new Vector2(x, y)));
+                }
+            }
+
+            return availableConnections;
+        }
+
+        public void AStar(Vector2 start, Vector2 end)
+        {
+            Dictionary<Vector2, (float, Vector2)> costs = new Dictionary<Vector2, (float, Vector2)>();
+
+            costs.Add(start, (0, new Vector2(-1, -1)));
+
+            List<(Vector2, float, Vector2)> currentConnections = new List<(Vector2, float, Vector2)>();
+
+            currentConnections.AddRange(getConnections(start));
+
+            while (currentConnections.Count > 0)
+            {
+                (Vector2, float, Vector2) lowestEdge = (new Vector2(-1, -1), float.MaxValue, new Vector2(-1, -1));
+                float lowestCost = float.MaxValue;
+
+                for (int c = 0; c < currentConnections.Count; c++)
+                {
+                    (Vector2, float, Vector2) connection = currentConnections[c];
+                    float cost = connection.Item2 + costs[connection.Item1].Item1 + ManhattanDistance(connection.Item3, end);
+                    if (cost < lowestCost)
+                    {
+                        if (costs.ContainsKey(connection.Item3))
+                        {
+                            currentConnections.Remove(connection);
+                            c--;
+                            continue;
+
+                        }
+                        lowestEdge = connection;
+                        lowestCost = cost;
+                    }
+                }
+
+                currentConnections.Remove(lowestEdge);
+                costs.Add(lowestEdge.Item3, (lowestCost, lowestEdge.Item1));
+                currentConnections.AddRange(getConnections(lowestEdge.Item3));
+            }
+            Vector2 currentSpot = costs[end].Item2;
+            while(currentSpot != start)
+            {
+                overLaySpots.Add(currentSpot);
+                currentSpot = costs[currentSpot].Item2;
+            }
+        }
+
+        public float ManhattanDistance(Vector2 start, Vector2 target)
+        {
+            float distance = Math.Abs(target.X - start.X) + Math.Abs(target.Y - target.Y);
+            return distance;
+        }
+    }
+}
---
+++ MazeTraversal/MazeTraversal/Game1.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MazeTraversal
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        MouseState ms;
+        KeyboardState ks;
+        Board board = new Board(10, 10, 50);
+        int screenWidth = 500;
+        int screenHeight = 500;
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        bool pressed = false;
+        byte selectedPointType = 1;
+        byte pressedPointType = 0;
+        byte drawing = 0;
+        Texture2D[] textures = new Texture2D[4];
+
+        int timeTillClear = -1;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            graphics.PreferredBackBufferWidth = screenWidth;
+            graphics.PreferredBackBufferHeight = screenHeight;
+            graphics.ApplyChanges();
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            textures[0] = Content.Load<Texture2D>("Zero");
+            textures[1] = Content.Load<Texture2D>("One");
+            textures[2] = Content.Load<Texture2D>("Two");
+            textures[3] = Content.Load<Texture2D>("Three");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            ms = Mouse.GetState();
+            ks = Keyboard.GetState();
+
+            Keys[] keys = ks.GetPressedKeys();
+            foreach(Keys key in keys)
+            {
+                if (key > Keys.D0 && key < Keys.D9)
+                {
+                    selectedPointType = (byte) (key - Keys.D0);
+                }
+                if(key == Keys.Enter)
+                {
+                    board.overLaySpots.Clear();
+                    board.AStar(board.findStart(), board.findEnd());
+                    timeTillClear = 5;
+                }
+            }
+
+            if(ms.LeftButton == ButtonState.Pressed)
+            {
+                int x = ms.X / board.size;
+                int y = ms.Y / board.size;
+
+                if (x < 0 || y < 0 || ms.X > screenWidth || ms.Y > screenWidth)
+                {
+                    base.Update(gameTime);
+                    return;
+                }
+
+                byte typeOfPressed = board.spots[x, y];
+                if (pressed)
+                {
+                    if (typeOfPressed == pressedPointType)
+                    {
+                        board.spots[x, y] = drawing;
+                    }
+                }
+                else
+                {
+                    pressed = true;
+                    if(typeOfPressed != 0)
+                    {
+                        pressedPointType = typeOfPressed;
+                        drawing = 0;
+                        board.spots[x, y] = 0;
+                    }
+                    else
+                    {
+                        pressedPointType = 0;
+                        drawing = selectedPointType;
+                        board.spots[x, y] = drawing;
+                    }
+                }
+            }
+            else
+            {
+                pressed = false;
+            }
+
+            if(timeTillClear != -1)
+            {
+                timeTillClear -= gameTime.ElapsedGameTime.Seconds;
+            }
+            else if(timeTillClear <= 0)
+            {
+                board.overLaySpots.Clear();
+                timeTillClear = -1;
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            for(int x = 0; x < board.width; x++)
+            {
+                for(int y = 0; y < board.height; y++)
+                {
+                    spriteBatch.Draw(textures[board.spots[x, y]], new Vector2(x * board.size, y * board.size), Color.White);
+                }
+            }
+            foreach(Vector2 position in board.overLaySpots)
+            {
+                spriteBatch.Draw(textures[0], position, Color.Blue);
+            }
+            base.Draw(gameTime);
+            spriteBatch.End();
+        }
+    }
+}
